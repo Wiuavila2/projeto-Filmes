aba43f5eaa20cf6a47fa2ce93a1d4b6fad558c74
WiuAvila2 <wiuneivetom.avila@outlook.com>
2025-04-05 16:37:36 -0300

    listando filmes (css da lista)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 function Home(){
     const [filmes ,setFilmes] = useState([]);
 
@@ -20,8 +22,18 @@ function Home(){
     }, [])
 
     return(
-        <div>
-            <h1>Bem vindo a Home</h1>
+        <div className='container'>
+           <div className='lista-filmes'>
+            {filmes.map((filme)=>{
+                return(
+                    <article key={filme.id}>
+                        <strong>{filme.title}</strong>
+                        <img src={'https://image.tmdb.org/t/p/original/${filme.poster_path}'} alt={filme.title} />
+                        <link to={'/filme/${filme.id}'}>Acessar</link>
+                    </article>
+                )
+            })}
+           </div>
         </div>
     )
     
