3e2973aca42c5f1881db33e95b2e34885b00a412
WiuAvila2 <wiuneivetom.avila@outlook.com>
2025-04-05 16:02:50 -0300

    teste retorno api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
 function Home(){
+    const [filmes ,setFilmes] = useState([]);
+
+    useEffect(()=>{
+        
+        
+        async function loadFilmes(){
+            const response = await api.get('/movie/now_playng',{
+                params:{
+                    api_key:'',
+                    lenguage: 'pt-BR',
+                    page: 1,
+                }
+            })
+        }
+        loadFilmes();
+            
+    }, [])
+
     return(
         <div>
             <h1>Bem vindo a Home</h1>
