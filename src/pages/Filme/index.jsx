09d891af4440b0e402927f6bbdf1507922a74532
WiuAvila2 <wiuneivetom.avila@outlook.com>
2025-04-27 19:08:39 -0300

    detalhes dos filmes (sem estilo)

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,7 +1,50 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import api from '../../services/api'
+
 function Filme(){
+    const{ id } = useParams();
+    const [filme,setFilme] = useState({});
+    const [loading,setLoading] = useState(true)
+    useEffect(()=>{
+        async function loadFilme(){
+        await api.get('/movie/${id}' , {
+            params:{
+                api_key:'',
+                lenguage: 'pt-BR',
+            }
+        })
+        .then((response)=>{
+            setFilme(response.data);
+            setLoading(false);
+        })
+        .catch(()=>{
+            console.log("filme não encontrado!")
+        })
+        }
+        loadFilme();
+        return()=>{
+
+        }
+    }, [])
+
+    if(loading){
+        return(
+            <div className='filmes-info'>
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
     return(
-        <div>
-            <h1>Bem vindo a Pagina detalhes do filme</h1>
+        <div className='filmes-info'>
+            <h1>{filme.title}</h1>
+            <img src={'https://image.tmdb.org/t/p/original/${filme.backdrop_path}'} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>  
+
+            <strong>Avaliação: {filme.vote_average} /10</strong>
         </div>
     )
     
